21147d2f13f800f5271b9bd5f499837c82ecf870
Thomas Levine <occurrence@thomaslevine.com>
2013-06-13 13:26:04 -0700

    import data

---
+++ music.r
@@ -0,0 +1,22 @@
+#!/usr/bin/env Rscript
+
+socrata <- (function() {
+  d <- read.csv('socrata.csv', stringsAsFactors = T)
+
+  # Characters
+  for (key in c('description', 'name', 'id', 'tags')) {
+    d[,key] <- as.character(d[,key])
+  }
+
+  # Dates
+  for (key in c('createdAt', 'publicationDate', 'viewLastModified', 'rowsUpdatedAt')) {
+    d[,key] <- as.POSIXct(d[,key], origin = '1970-01-01')
+    d[,paste(key,'day',sep='.')] <- as.Date(d[,key])
+  }
+
+  d
+})()
+
+
+p.1 <- ggplot(socrata) + aes(x = createdAt.day, group = portal, fill = portal) + geom_bar()
+
